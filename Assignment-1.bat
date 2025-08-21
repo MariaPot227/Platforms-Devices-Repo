6f3c7bce6205a5cfefc1b162d8dbb73eb2e47af5
Maria Potapov (1000136837) <POTAM2@student.op.ac.nz>
2025-08-22 08:46:01 +1200

    adding the template of A2

--- Assignment-1.bat
+++ Assignment-1.bat
@@ -3,7 +3,15 @@
 ::Maria Potapov, 1000136837
 ::Platforms and Devices, 2025, Semester 2
 @ECHO off
+::---------------------//----------------------
 ECHO.
 ECHO A1. Output a ine to the screen that displays your name using the ECHO command which displays text to the screen
 ECHO Maria Potapov
 PAUSE 
+CLS
+::---------------------//----------------------
+ECHO.
+ECHO A2. Output the file name to the screen (not hard coded) using ECHO and 
+ECHO Maria Potapov
+PAUSE 
+CLS
