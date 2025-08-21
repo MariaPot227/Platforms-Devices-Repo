05d0cc7d484c71dc7a547f9523c4d76ed51fb710
Maria Potapov (1000136837) <POTAM2@student.op.ac.nz>
2025-08-22 08:38:01 +1200

    A1 finished

--- Assignment-1.bat
+++ Assignment-1.bat
@@ -1,3 +1,9 @@
+::File name is Assignment-1.bat
+::This script carries out assignment 1
+::Maria Potapov, 1000136837
+::Platforms and Devices, 2025, Semester 2
 @ECHO off
-ECHO Hello, World
-PAUSE
+ECHO.
+ECHO A1. Output a ine to the screen that displays your name using the ECHO command which displays text to the screen
+ECHO Maria Potapov
+PAUSE 
