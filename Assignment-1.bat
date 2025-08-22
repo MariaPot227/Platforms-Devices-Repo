50d77208eaf7e8a8793fa1f34285533e880f8fc8
unknown <potam2@student.op.ac.nz>
2025-08-22 12:54:58 +1200

    done up to A11

--- Assignment-1.bat
+++ Assignment-1.bat
@@ -11,7 +11,63 @@ PAUSE
 CLS
 ::---------------------//----------------------
 ECHO.
-ECHO A2. Output the file name to the screen (not hard coded) using ECHO and 
-ECHO Maria Potapov
+ECHO A2. Output the file name to the screen (not hard coded) using ECHO
+ECHO %~nx0
 PAUSE 
 CLS
+::---------------------//----------------------
+ECHO.
+ECHO A3. Create folder on root of the C:drive called "My Batch Script File Assignment" using mkdir
+MKDIR "C:\My Batch Script File Assignment"
+PAUSE 
+CLS
+::---------------------//----------------------
+ECHO.
+ECHO A4. Create folder within "My Batch Script File Assignment" called "Input" using mkdir
+MKDIR "C:\My Batch Script File Assignment\Input"
+PAUSE 
+CLS
+::---------------------//----------------------
+ECHO.
+ECHO A5. Create another folder within "My Batch Script File Assignment" called "Processing" using mkdir
+MKDIR "C:\My Batch Script File Assignment\Processing"
+PAUSE 
+CLS
+::---------------------//----------------------
+ECHO.
+ECHO A6. Create another folder within "My Batch Script File Assignment" called "Output" using mkdir
+MKDIR "C:\My Batch Script File Assignment\Output"
+PAUSE 
+CLS
+::---------------------//----------------------
+ECHO.
+ECHO A7. List all hidden files in the root directory of the C:drive - output the listing to a file called "Input Data.txt" in the "Input" subfolder using dir to look inside container, and /a:h to ask for hidden files 
+DIR C:\ /a:h > "C:\My Batch Script File Assignment\Input\Input Data.txt"
+PAUSE 
+CLS
+::---------------------//----------------------
+ECHO.
+ECHO A8. Make a backup copy of "Input Data.txt" on the root of the C:drive, and with the same name, but with extension ".bak" using copy
+COPY "C:\My Batch Script File Assignment\Input\Input Data.txt" "C:Input Data.bak"
+PAUSE 
+CLS
+::---------------------//----------------------
+ECHO.
+ECHO A9. Go to directory of the C:drive using cd
+CD /d C:\
+PAUSE 
+CLS
+::---------------------//----------------------
+ECHO.
+ECHO A10. Update the folder search path for batch script file execution to include the "Processing" subfolder, and then display the folder search path using set to append folder to an existing path, and echo to print the updated path 
+SET "PATH=%PATH%;C:\My Batch Script File Assignment\Processing"
+ECHO %PATH%
+PAUSE 
+CLS
+::---------------------//----------------------
+ECHO.
+ECHO A11. Change the command prompt to include the time, the words "Hello, World" and the '>' character using prompt, with T for time and G for >
+ECHO %time% Hello World>
+PAUSE 
+CLS
+
