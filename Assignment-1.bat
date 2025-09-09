53faae3e44ab2797e31e756afacda2f592eeb6a5
unknown <potam2@student.op.ac.nz>
2025-09-10 07:52:35 +1200

    up to Q18

--- Assignment-1.bat
+++ Assignment-1.bat
@@ -65,12 +65,14 @@ ECHO %PATH%
 PAUSE 
 CLS
 ::---------------------//----------------------
+::@ECHO on
 ::ECHO.
 ::ECHO A11. Change the command prompt to include the time, the words "Hello, World" and the '>' character using prompt, with T for time and G for >
-::ECHO %time% Hello World>
+::PROMPT $t Hello, World$g
 ::PAUSE 
 ::CLS
 ::---------------------//----------------------
+@ECHO off
 ECHO.
 ECHO A12. Create a new command window, with red coloured text and green background using cmd, color and /k
 START cmd /k color 2C
@@ -85,7 +87,33 @@ CLS
 ::---------------------//----------------------
 ECHO.
 ECHO A14. List all folders in C:WINDOWS\System32, sorted into alphabetical order - output listing to a file called "Batch Script File Output Data.txt" in "Output" subfolder using DIR
-DIR "C:\WINDOWS\System32" /ad /b |sort > "C:\Batch Script File Assignment\Output\Batch Script
+DIR "C:\WINDOWS\System32" /ad /b |sort > "C:\My Batch Script File Assignment\Output\Batch Script File Output Data.txt"
+PAUSE 
+CLS
+::---------------------//----------------------
+ECHO.
+ECHO A15. List all text files whose names are up to seven characters long on the whole C: drive (wide format)the listing output must be appended to the end of “Batch Script File Output Data.txt”
+DIR C:\???????.txt /s /w >> "Batch Script File Output Data.txt"
+PAUSE 
+CLS
+::---------------------//----------------------
+ECHO.
+ECHO A16. Delete folder “My Batch Script File Assignment”, together with all subfolders and their contents. 
+RMDIR /s "My Batch Script File Assignment"
+PAUSE 
+CLS
+::---------------------//----------------------
+ECHO.
+ECHO A17. Output to the screen the configuration information relating to your network settings using ipconfig
+IPCONFIG
+PAUSE 
+CLS
+::---------------------//----------------------
+ECHO.
+ECHO A18. Create a local user called “Bob”, then create a local group called “Awesome Users” and add Bob to it. 
+NET user Bob /add
+NET localgroup "Awesome Users" /add
+NET localgroup "Awesome Users" Bob /add
 PAUSE 
 CLS
 
